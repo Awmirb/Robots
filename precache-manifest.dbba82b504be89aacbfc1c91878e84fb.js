bbf95edb6ebc3fe3b42c44e5dc238e9105a25a35
Amir <amirhossein.b2829@yahoo.com>
2020-10-21 16:27:35 +0330

    Updates

---
+++ precache-manifest.dbba82b504be89aacbfc1c91878e84fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc1916d0aee46945ed0185210e52fc1",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "50d7deee29c579499fc6",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "96ee66c1fa54ab362e5a",
+    "url": "/Robots/static/css/main.80a3ced8.chunk.css"
+  },
+  {
+    "revision": "50d7deee29c579499fc6",
+    "url": "/Robots/static/js/2.e204960b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Robots/static/js/2.e204960b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ee66c1fa54ab362e5a",
+    "url": "/Robots/static/js/main.79f8dac2.chunk.js"
+  },
+  {
+    "revision": "611aa5a5f21921c7c534",
+    "url": "/Robots/static/js/runtime-main.cb291d75.js"
+  },
+  {
+    "revision": "449eadc07f3f015052a49d4cd4fcf86e",
+    "url": "/Robots/static/media/blops.449eadc0.woff"
+  }
+]);
